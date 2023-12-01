abf4e9634fda0c9bb416dbf60217e55a53c0198e
Milan Bozic <Bozic411@gmail.com>
2023-12-01 18:39:54 +0100

    Toggle Switch | Font Color & Handler Event

--- src/@ui/Toggle/Toggle.js
+++ src/@ui/Toggle/Toggle.js
@@ -1,12 +1,12 @@
 import React from "react";
 import classes from "./Toggle.module.scss";
 
-const Toggle = ({ children, checked }) => {
+const Toggle = ({ children, checked, onClick }) => {
   return (
     <div className={classes.toggle}>
       <p className={classes.label}>{children}</p>
       <label className={classes.switch}>
-        <input type="checkbox" checked={checked} />
+        <input onClick={onClick} type="checkbox" checked={checked} />
         <span className={`${classes.slider} ${classes.round}`}></span>
       </label>
     </div>
