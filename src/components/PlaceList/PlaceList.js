bd57fa73db4b9edb8bbbd1b915379b028eb5f773
Milan Bozic <Bozic411@gmail.com>
2023-12-03 15:57:02 +0100

    Error Exceptions

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -12,12 +12,38 @@ import useInput from "hooks/useInput";
 
 const PlaceList = ({ places, isLoading, getLocation }) => {
   const [bool, setbool] = useState(false);
-  const [search, setSearch] = useInput("kafana");
+  const [search, setSearch] = useInput("");
+
+  const filterList = () => {
+    return places.filter(({ name }) => name.toLowerCase().includes(search));
+  };
 
   const onChangeClick = () => {
     console.log("click");
     setbool((prev) => !prev);
   };
+
+  let content = null;
+
+  if (isLoading) {
+    content = <LoadingSpinner />;
+  } else if (places.length === 0) {
+    content = (
+      <div className={classes.message}>
+        <p>No places found in this area. Please try resizig map.</p>
+      </div>
+    );
+  } else if (places.length > 0 && filterList().length > 0) {
+    content = filterList().map((place) => (
+      <PlaceCard title={place.name} address={place.address} place={place} />
+    ));
+  } else {
+    content = (
+      <div className={classes.message}>
+        <p>No places found for phrase "{search}". Please try another phrase.</p>
+      </div>
+    );
+  }
 
   return (
     <div className={classes.list}>
@@ -35,18 +61,7 @@ const PlaceList = ({ places, isLoading, getLocation }) => {
           onClick={getLocation}
         />
       </div>
-      <div className={classes.cards}>
-        {isLoading && <LoadingSpinner />}
-        {places
-          .filter(({ name }) => name.toLowerCase().includes(search))
-          .map((place) => (
-            <PlaceCard
-              title={place.name}
-              address={place.address}
-              place={place}
-            />
-          ))}
-      </div>
+      <div className={classes.cards}>{content}</div>
     </div>
   );
 };
