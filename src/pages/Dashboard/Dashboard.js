697599e8ad2193e2d84a5bf46786454bbc6d2740
Milan Bozic <Bozic411@gmail.com>
2023-12-04 22:13:04 +0100

    Added: Dashboard

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Dashboard.module.scss";
+
+const Dashboard = () => {
+  return <div className={classes.wrapper}></div>;
+};
+
+export default Dashboard;
